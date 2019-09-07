53ad4be79e04c33f866a574bbd00c0b04d6c0315
deezy13 <deezy13@gmail.com>
2019-09-07 18:05:26 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,18 @@
+
+
 def square_array(array)
   # your code here
-end
+  
+  index = 0
+  while (array[index]) do
+    array[index] = array[index] ** 2
+        puts array[index]
+             index += 1
+    end
+    
+   return array
+   
+  end
+  
+  numbers = [1,2,3]
+  square_array(numbers)
